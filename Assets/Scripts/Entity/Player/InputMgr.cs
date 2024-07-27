52beb48bac74ea21cdfd06c9c08a37701b2f6e90
k2sss <2118968895@qq.com>
2024-07-27 10:13:34 +0800

    角色控制器

---
+++ Assets/Scripts/Entity/Player/InputMgr.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputMgr
+{
+    private static bool isActive = true;
+    public static float GetHorizontal()
+    {
+        if (!isActive) return 0;
+        return Input.GetAxis("Horizontal");
+    }
+    public static float GetVertical()
+    {
+        if (!isActive) return 0;
+        return Input.GetAxis("Vertical");
+    }
+    public static bool GetSpace()
+    {
+        if (!isActive) return false;
+        return Input.GetKey(KeyCode.Space);
+    }
+    public static void Enable()
+    {
+        isActive = true;
+    }
+    public static void Disable()
+    {
+        isActive = false;
+    }
+  
+}
---
+++ Assets/Scripts/Entity/Player/MoveController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public float MoveSpeed { get { return moveSpeed; } set { moveSpeed = value; } }
+    private float moveSpeed;
+    public float JumpForce = 5;
+    private float jumpTimer;
+    private float jumpCD = 0.1f;
+    private Rigidbody2D rb;
+    private bool isGround;
+    
+    public Transform[] rayCheckPoints;
+    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+      
+    }
+    private void Start()
+    {
+        //Test
+        MoveSpeed = 5;  
+        jumpTimer = Time.time;
+    }
+    private void Update()
+    {
+        CheckIsGround();
+        JumpLogic();
+        
+    }
+    private void FixedUpdate()
+    {
+        HorizontalMove(InputMgr.GetHorizontal());
+        
+    }
+    public void HorizontalMove(float value)
+    {
+        rb.velocity = new Vector2(value * moveSpeed,rb.velocity.y);
+        
+    }
+    /// <summary>
+    /// 检测角色是否在地面上
+    /// </summary>
+    /// <returns></returns>
+    private bool CheckIsGround()
+    {
+        for (int i = 0; i < rayCheckPoints.Length; i++)
+        {
+            if (Physics2D.Raycast(rayCheckPoints[i].position, Vector2.down, 0.1f,~(1<<3)))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    /// <summary>
+    /// handle jump logic
+    /// </summary>
+   
+    public void JumpLogic()
+    {
+      
+        if (InputMgr.GetSpace() && CheckIsGround()&& jumpTimer + jumpCD <= Time.time)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+            jumpTimer = Time.time;
+        }
+    }
+}
