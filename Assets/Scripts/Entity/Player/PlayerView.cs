d12829c805b665d79ed092f1ddb346ef4ca8da9a
k2sss <2118968895@qq.com>
2024-07-30 09:19:31 +0800

    环境粒子

--- Assets/Scripts/Entity/Player/PlayerView.cs
+++ Assets/Scripts/Entity/Player/PlayerView.cs
@@ -6,13 +6,24 @@ public class PlayerView : MonoBehaviour
 {
     public GameObject viewObject;
     private float FlipTimer;
-
+    private PlayerMgr mgr;
+    public GameObject landParticlePrefab;
+    public void Init(PlayerMgr mgr)
+    {
+        this.mgr = mgr;
+        mgr.moveController.onLandAction += () =>
+        {
+            GameObject go = ObjectPool.Instance.GetObject(landParticlePrefab);
+            go.transform.position = viewObject.transform.position +Vector3.up * 0.1f;
+        };
+    }
     private void Start()
     {
         FlipTimer = Time.time;
     }
     public void Flip(float dir)
     { 
+        
         const float FlipTime = 0.2f;
         
         if (FlipTimer + FlipTime > Time.time)
@@ -31,4 +42,6 @@ public class PlayerView : MonoBehaviour
             viewObject.transform.DOScaleX(-1, FlipTime);
         }
    }
+    
+
 }
